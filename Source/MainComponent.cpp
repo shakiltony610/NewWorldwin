5639b47522005289f597a2415263c07933d172b3
tony610 <shakiltony610215@outlook.com>
2022-02-14 15:22:08 +0800

    Change text

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -17,8 +17,8 @@ void MainComponent::paint (juce::Graphics& g)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setFont (juce::Font (32.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setColour (juce::Colours::red);
+    g.drawText ("Hello World! Hi I am Tony", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
